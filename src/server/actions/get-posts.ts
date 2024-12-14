1800ed8cef44881cb74aa168477e0a4ff8fa9813
pouyatayebi <tayebi.pouya@gmail.com>
2024-12-14 23:08:00 +0330

    install neondb

---
+++ src/server/actions/get-posts.ts
@@ -0,0 +1,11 @@
+'use server'
+
+import {db} from "@/server"
+
+export async function getPosts() {
+    const posts=db.query.posts.findMany()
+    if(!posts){
+        return {error:"no posts found"}
+    }
+    return {success:posts}
+}
---
+++ src/server/migrations/0000_fat_human_cannonball.sql
@@ -0,0 +1,4 @@
+CREATE TABLE "posts" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"text" text NOT NULL
+);
