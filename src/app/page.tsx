35dd3a4086b7fa85229d392003b993a2bc4a1384
pouyatayebi <tayebi.pouya@gmail.com>
2024-12-14 23:09:47 +0330

    install neondb-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,8 @@
 import { getPosts } from "@/server/actions/get-posts";
-import Image from "next/image";
 
 export default async function Home() {
+  const data = await getPosts();
+  console.log("data", data);
 
-  const data=await getPosts()
-  console.log("data",data)
-
-
-    return(
-      <main>
-       main
-        
-      </main>
-    );
-  }
- 
-  
+  return <main>main</main>;
+}
